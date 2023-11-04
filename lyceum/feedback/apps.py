8bbe7f48e1e7d8b9272c3bafd3adec23056ae46c
ITQ <118541411+DevItq@users.noreply.github.com>
2023-11-04 19:40:24 +0300

    Fix for LMS

---
+++ lyceum/feedback/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+__all__ = ("FeedbackConfig",)
+
+
+class FeedbackConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "feedback"
