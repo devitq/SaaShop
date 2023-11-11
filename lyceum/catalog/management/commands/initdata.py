c265cd22d91525621a2e5e11d1abbd208863ae15
ITQ <118541411+DevItq@users.noreply.github.com>
2023-11-11 15:45:43 +0300

    Some improvements 2

--- lyceum/catalog/management/commands/initdata.py
+++ lyceum/catalog/management/commands/initdata.py
@@ -8,50 +8,32 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
             "--interactive",
-            action="store_true",
+            type=str,
             help=(
                 "Input superuser data in console"
                 "instead of getting it from env"
             ),
         )
-        parser.add_argument(
-            "--skip-applying-migrations",
-            action="store_true",
-            help=("Add this to stop loading migrations"),
-        )
-        parser.add_argument(
-            "--skip-loading-fixtures",
-            action="store_true",
-            help=("Add this to stop loading fixtures"),
-        )
-        parser.add_argument(
-            "--skip-creating-superuser",
-            action="store_true",
-            help=("Add this to stop creating superuser"),
-        )
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS("Applying migrations..."))
-        if not options["skip_applying_migrations"]:
-            call_command("migrate")
-            self.stdout.write(self.style.SUCCESS("Loaded"))
-        else:
-            self.stdout.write(self.style.WARNING("Skipped"))
+        call_command("migrate")
+        self.stdout.write(self.style.SUCCESS("Loaded"))
 
         self.stdout.write(self.style.SUCCESS("Loading fixtures..."))
-        if not options["skip_loading_fixtures"]:
-            call_command("loaddata", "fixtures/data.json")
-            self.stdout.write(self.style.SUCCESS("Loaded"))
-        else:
-            self.stdout.write(self.style.WARNING("Skipped"))
+        call_command("loaddata", "fixtures/data.json")
+        self.stdout.write(self.style.SUCCESS("Loaded"))
 
         self.stdout.write(self.style.SUCCESS("Creating superuser..."))
-        if not options["skip_creating_superuser"]:
-            if options["interactive"]:
-                call_command("createsuperuser")
-            else:
-                call_command("createsuperuser", interactive=False)
+        if options["interactive"] and options["interactive"].lower() in (
+            "yes",
+            "y",
+            "true",
+            "t",
+            "1",
+        ):
+            call_command("createsuperuser")
         else:
-            self.stdout.write(self.style.WARNING("Skipped"))
+            call_command("createsuperuser", interactive=False)
 
         self.stdout.write(self.style.SUCCESS("Ended successfully"))
